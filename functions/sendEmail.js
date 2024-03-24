c352c14802ebc6cd168b0f9d2f0bc69c59f0f484
Abhishek Kumar <56792136+abhishek1637@users.noreply.github.com>
2024-03-24 19:16:36 +0000

    use netlify backend

---
+++ functions/sendEmail.js
@@ -0,0 +1,43 @@
+// functions/sendEmail.js
+
+const nodemailer = require('nodemailer');
+
+exports.handler = async function (event, context) {
+  const { firstName, lastName, email, phone, message } = JSON.parse(event.body);
+
+  // Create Nodemailer transporter
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  // Compose email
+  const mailOptions = {
+    from: `${firstName} ${lastName}`,
+    to: process.env.EMAIL_USER,
+    subject: 'Contact Form Submission - Portfolio',
+    html: `
+      <p>Name: ${firstName} ${lastName}</p>
+      <p>Email: ${email}</p>
+      <p>Phone: ${phone}</p>
+      <p>Message: ${message}</p>
+    `,
+  };
+
+  // Send email
+  try {
+    await transporter.sendMail(mailOptions);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Email sent successfully' }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed to send email' }),
+    };
+  }
+};

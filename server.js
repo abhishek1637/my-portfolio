dd3986b528973d7bc580d116d67aec8d128a733e
Abhishek Kumar <56792136+abhishek1637@users.noreply.github.com>
2024-03-24 18:53:33 +0000

    feat(server): update server file

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 
+const corsOptions = {
+  origin: 'https://abhishek-work.netlify.app',
+};
+
 // server used to send send emails
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/", router);
 app.listen(5000, () => console.log("Server Running"));

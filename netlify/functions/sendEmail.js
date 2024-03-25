519833bbbda6e81d5f8367dfe275317992453276
Abhishek Kumar <56792136+abhishek1637@users.noreply.github.com>
2024-03-25 03:47:18 +0000

    feat(portfolio): update sendEmail with smtp

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -1,28 +1,32 @@
 // functions/sendEmail.js
 
 const nodemailer = require('nodemailer');
+const smtpTransport = require('nodemailer-smtp-transport');
 
 exports.handler = async function (event, context) {
   const { firstName, lastName, email, phone, message } = JSON.parse(event.body);
 
   // Create Nodemailer transporter
-  const transporter = nodemailer.createTransport({
+  const transporter = nodemailer.createTransport(smtpTransport({
     service: 'gmail',
+    host: 'smtp.gmail.com',
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
-  });
+  }));
 
-  console.log('username', process.env.EMAIL_USER);
+  const sendEmailId = process.env.EMAIL_USER;
+
+  console.log('username', sendEmailId);
   console.log('password', process.env.EMAIL_PASS);
 
   // Compose email
   const mailOptions = {
     from: `${firstName} ${lastName}`,
-    to: process.env.EMAIL_USER,
+    to: sendEmailId,
     subject: 'Contact Form Submission - Portfolio',
-    html: `
+    text: `
       <p>Name: ${firstName} ${lastName}</p>
       <p>Email: ${email}</p>
       <p>Phone: ${phone}</p>
@@ -38,6 +42,7 @@ exports.handler = async function (event, context) {
       body: JSON.stringify({ message: 'Email sent successfully' }),
     };
   } catch (error) {
+    console.log(error);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: 'Failed to send email' }),
